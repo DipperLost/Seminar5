45cbccbcde30a0df4db03eecd8ad8777fe3eed15
dipperlost <dipper>
2022-08-30 10:22:26 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[5];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("число четных чисел в массиве" + " " + count);
