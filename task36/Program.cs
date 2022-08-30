00ef5c7857b2ae9a58589eee99ab485c6acab995
dipperlost <dipper>
2022-08-30 10:31:08 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[10];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 10000);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 > 0)
+    {
+        count = count + array[i];
+        count++;
+    }
+}
+Console.WriteLine("сумма элементов, стоящих на нечётных позициях"
+ + " " + count);
