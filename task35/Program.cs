a71628da4a89246ac3b8a63bfd8a844e07c2acd3
dipperlost <dipper>
2022-08-30 10:14:16 +0300

    task35

---
+++ task35/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[123];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 1000);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(10 <= array[i]&& array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
