0eefe0ee6559e5bd04330f1326d6118d50da50ec
dipperlost <dipper>
2022-08-30 11:08:37 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,28 @@
+﻿double[] array = new double[5];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+double number = Convert.ToDouble(Console.ReadLine());
+    array[i] = number;
+}
+Console.WriteLine('[' + string.Join("; ", array) + ']');
+double Max = array[0];
+for(int i = 1; i < array.Length; i++)
+{
+    if(array[i] > Max);
+        array[i] = Max;
+}
+Console.WriteLine("максимальный элемент массива"
+ + " " + Max);
+double Min = array[0];
+for(int i = 1; i < array.Length; i++)
+{
+    if(array[i] < Min);
+        Min = array[i];
+}
+Console.WriteLine("минимальный элемент массива"
+ + " " + Min);
+double result = Max - Min;
+Console.WriteLine("разница между максимальным и минимальным элементов массива"
+ + " " + result);
